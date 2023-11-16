8186c5666085803f88f50a4acb19c5c2d07f6fa3
shrutithakare <94057617+shrutithakare@users.noreply.github.com>
2023-11-16 15:08:25 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const server = http.createServer((req , res) => {
+    console.log('New connection')
+    res.end('Hello awesome')
+})
+
+const PORT = process.env.PORT || 8080
+server.listen(PORT , () => console.log('listening'))
